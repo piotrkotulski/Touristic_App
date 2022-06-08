49c0ee21e1da9c7663f544ac22d1c537816b135a
Dzejkop <93822157+Kojaro21@users.noreply.github.com>
2022-06-08 12:21:58 +0200

    Update Program.cs

--- Touristic_App/Program.cs
+++ Touristic_App/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Touristic_App
 {
@@ -6,7 +6,42 @@ namespace Touristic_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Witamy w biurze turystycznym Wakacje z WSB! \n  "); 
+                Console.WriteLine("Wybierz proszę kierunek podróży, który Cię interesuje: ");
+
+
+            var Kierunek1 = Hiszpania;
+            var Kierunek2 = Grecja;
+            var Kierunek3 = Portugalia;
+
+
+            if (Kierunek1 = )
+            {
+
+            }
+            else
+                Console.WriteLine("Nie odnaleziono takiego kraju");
+            if (Kierunek2 = )
+            {
+
+            }
+            else
+                Console.WriteLine("Nie odnaleziono takiego kraju");
+
+            if (Kierunek3 = )
+            {
+
+            }
+            else
+                Console.WriteLine("Nie odnaleziono takiego kraju");
+
+
+
+            // forma przywitania i strony tytulowej potem wybór dokąd chce polecieć zrobimy z 3 czy 4 kierunki (wybor switchem/ifem)
+            // następnie się zrobi wybór kilku hoteli (swotch if)deklaracja ile osób - wybór all inclusive/ultra(wybór i później cena)
+            // na końcu będzie musiało wydrukować zamówienie z ceną łączną jeszcze nie myślałem jak to ugryzc co z czego ma dziedziczyć .
+            // musi być klasa tworzaca klienta  i kakies interfacy z opcjami i metodami
+            // Tutaj dodaje koncepcje Piotra i troche tam swojego dodałem, jeśli macie jakieś swoje adnotacje to dodajcie i można zacząć działać :)
         }
     }
 }
